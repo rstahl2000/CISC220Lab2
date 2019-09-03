4a4af3cf22e8d1fe58d2ef867f53aee519fc016d
Robert Stahl <rstahl@udel.edu>
2019-09-03 14:40:23 -0400

    Func 2

--- Lab 2/Lab2.cpp
+++ Lab 2/Lab2.cpp
@@ -12,6 +12,7 @@ using namespace std;
 
 
 void func1();
+void func2(int y);
 
 int main(){
 	func1();
@@ -23,4 +24,9 @@ void func1(){
 	int x=2;
 	cout<<x<<endl;
 	cout<<&x<<endl;
+}
+
+void func2(int y){
+	y=y+4;
+
 }
